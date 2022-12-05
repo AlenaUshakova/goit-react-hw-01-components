19398909da3ba018e5e30d19767793062fdf0bf7
AlenaUshakova <alenushakova@gmail.com>
2022-12-05 15:22:55 +0200

    edd new files

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+// import styled from 'styled-components';
+// import PropTypes from 'prop-types';
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
+
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data.json';
+
+import { FriendList } from './FriendList/FriendList';
+import friends from './FriendList/friends.json';
+
+import { TransactionHistory } from './Transaction/Transaction';
+import transactions from './Transaction/transactions.json';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        flexDirection: 'column',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import {
+  StatisticsContainer,
+  ListTitle,
+  StatList,
+  StatListItem,
+  Label,
+  Percentage,
+} from './Statistics.styled';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <StatisticsContainer>
+      {title && <ListTitle>{title}</ListTitle>}
+      <StatList>
+        {stats.map(stat => (
+          <StatListItem
+            key={stat.id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <Label>{stat.label}</Label>
+            <Percentage>{stat.percentage}%</Percentage>
+          </StatListItem>
+        ))}
+      </StatList>
+    </StatisticsContainer>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
